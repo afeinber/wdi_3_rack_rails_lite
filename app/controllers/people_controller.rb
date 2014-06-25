e97e31eb654a536414c3838f922c1d9f5ddcd794
afeinber <alexandernfeinberg@gmail.com>
2014-06-25 15:16:27 -0400

    did the things

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,12 @@
+module PersonApp
+  class PeopleController < ApplicationController
+
+    def index
+      "In the PeopleController#index method"
+    end
+
+    def show
+      "In the PeopleController#show with params = #{params.inspect}"
+    end
+  end
+end
